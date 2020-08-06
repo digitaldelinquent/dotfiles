19f79e360b79be46706164bcdad6502002455389
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2020-08-05 21:59:32 -0700

    changed keybindings in penrose config

--- config/penrose/src/main.rs
+++ config/penrose/src/main.rs
@@ -3,7 +3,7 @@ extern crate penrose;
 
 use penrose::core::{Layout, WindowManager, XcbConnection};
 use penrose::layout::{bottom_stack, side_stack, LayoutConf};
-use penrose::{Backward, Config, Forward, Less, More};
+use penrose::{Backward, Config, Forward, More};
 
 use penrose::contrib::extensions::Scratchpad;
 use penrose::contrib::hooks::{LayoutSymbolAsRootName, RemoveEmptyWorkspaces};
@@ -42,39 +42,37 @@ fn main() {
 
     let key_bindings = gen_keybindings! {
         // Program launch
-        "M-semicolon" => run_external!("dmenu_run"),
+        "M-r" => run_external!("dmenu_run"),
         "M-Return" => run_external!("st"),
 
         // client management
-        "M-j" => run_internal!(cycle_client, Forward),
-        "M-k" => run_internal!(cycle_client, Backward),
-        "M-S-j" => run_internal!(drag_client, Forward),
-        "M-S-k" => run_internal!(drag_client, Backward),
-        "M-S-q" => run_internal!(kill_client),
+        "A-Tab" => run_internal!(cycle_client, Forward),
+        "A-S-Tab" => run_internal!(cycle_client, Backward),
+        "M-j" => run_internal!(drag_client, Forward),
+        "M-k" => run_internal!(drag_client, Backward),
+        "M-q" => run_internal!(kill_client),
         "M-slash" => sp.toggle(),
 
         "M-Tab" => run_internal!(toggle_workspace),
-        "M-bracketright" => run_internal!(cycle_screen, Forward),
-        "M-bracketleft" => run_internal!(cycle_screen, Backward),
-        "M-S-bracketright" => run_internal!(drag_workspace, Forward),
-        "M-S-bracketleft" => run_internal!(drag_workspace, Backward),
+        "C-S-A-l" => run_internal!(drag_workspace, Forward),
+        "C-S-A-h" => run_internal!(drag_workspace, Backward),
 
         // Layout management
         "M-grave" => run_internal!(cycle_layout, Forward),
         "M-S-grave" => run_internal!(cycle_layout, Backward),
         "M-A-Up" => run_internal!(update_max_main, More),
-        "M-A-Down" => run_internal!(update_max_main, Less),
-        "M-A-Right" => run_internal!(update_main_ratio, More),
-        "M-A-Left" => run_internal!(update_main_ratio, Less),
+        // "M-A-Down" => run_internal!(update_max_main, Less),
+        // "M-A-Right" => run_internal!(update_main_ratio, More),
+        // "M-A-Left" => run_internal!(update_main_ratio, Less),
 
-        "M-A-s" => run_internal!(detect_screens),
+        "C-m" => run_internal!(detect_screens),
 
-        "M-A-Escape" => run_internal!(exit);
+        "M-l" => run_internal!(exit);
 
         // setting up bindings for 6 possible workspaces
-        forall_workspaces: &[1, 2, 3, 4, 5, 6] => {
-            "M-{}" => focus_workspace,
-            "M-S-{}" => client_to_workspace,
+        forall_workspaces: &["v", "c", "x", "z"] => {
+            "M-A-{}" => focus_workspace,
+            "A-S-{}" => client_to_workspace,
         }
     };
 
