fa161aae00270576b155e0681ebae8089a999f99
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2023-12-17 02:58:48 -0800

    added suspend when locking screen via powermenu

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -6,8 +6,7 @@ opts=(
     [襤 Poweroff  ]="poweroff"
     [累 Reboot]="reboot"
     [ Logout]="bspc quit"
-    [ Lock]="i3lock -i $HOME/Pictures/lain.png"
-    [Sleep]="systemctl suspend"
+    [ Lock]="i3lock -i $HOME/Pictures/lain.png; systemctl suspend"
 )
 
 choice=$(printf "%s\n" "${!opts[@]}" | dmenu)
