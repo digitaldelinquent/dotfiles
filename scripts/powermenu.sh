d898a97d886d51911197ed0c6342f23b0c5825b8
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2024-01-02 00:08:48 -0800

    fixed powermenu script to have dmenu theming

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -9,6 +9,6 @@ opts=(
     [ Lock]="i3lock -i $HOME/Pictures/lain.png; systemctl suspend"
 )
 
-choice=$(printf "%s\n" "${!opts[@]}" | dmenu)
+choice=$(printf "%s\n" "${!opts[@]}" | dmenu -nb "#282A36" -nf "#ffffff" -sb "#b18ef9" -sf "#ffffff" -fn monospace -b)
 
 eval ${opts[$choice]}
