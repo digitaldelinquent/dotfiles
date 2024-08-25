49d7c017d189861cc2ab7a9655013ada484e4aa3
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2024-08-24 22:14:35 -0700

    enable netbird

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -144,7 +144,7 @@
         sxhkd # Keyboard shortcut daemon, built with BSPWM in mind
         transmission # Torrenting Linux ISOs
         wireguard-tools # Utilities for connecting and using wireguard VPNs
-        netbird # Homelab VPN
+        netbird-ui # Homelab VPN
         killall # Neat tool to kill processes by name
         usbutils # Contains lsusb
         zsh # Shell of choice
@@ -199,6 +199,9 @@
             alsa.support32Bit = true;
             pulse.enable = true;
         };
+
+        # Enable netbird
+        netbird.enable = true;
 
         # Configure Syncthing Service
         syncthing = {
