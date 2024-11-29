0c6ff4f474dfddaacd3ac54e5dbb66e789923e2e
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2024-11-29 09:49:45 -0800

    remove drag and drop option

--- hosts/comp22/default.nix
+++ hosts/comp22/default.nix
@@ -226,7 +226,6 @@
     virtualisation.virtualbox.host.enable = true;
     virtualisation.virtualbox.host.enableExtensionPack = true;
     virtualisation.virtualbox.guest.enable = true;
-    virtualisation.virtualbox.guest.dragAndDrop = true;
 
     # Enable flakes bc they cool
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
