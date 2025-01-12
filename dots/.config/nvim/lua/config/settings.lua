5d727504d6a99d1cf7bc882572b5ec1496ebefd8
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2025-01-12 02:41:29 -0800

    change color scheme declaration

--- dots/.config/nvim/lua/config/settings.lua
+++ dots/.config/nvim/lua/config/settings.lua
@@ -1,62 +1,62 @@
 -- Set theme
-vim.cmd[[colorscheme dracula]]
+vim.cmd.colorscheme('dracula')
 
 -- Show line numbers
 
-vim.opt.number=true
+vim.opt.number = true
 
 -- Show file stats
 
-vim.opt.ruler=true
+vim.opt.ruler = true
 
 -- Blink cursor on error instead of beeping (grr)
 
-vim.opt.visualbell=true
+vim.opt.visualbell = true
 
 -- Cursor line and column
 
-vim.opt.cursorline=true
-vim.opt.cursorcolumn=true
+vim.opt.cursorline = true
+vim.opt.cursorcolum = true
 
 -- Encoding
 
-vim.opt.encoding='utf-8'
+vim.opt.encoding = 'utf-8'
 
 -- clipboard copy/paste
 
-vim.opt.clipboard= {'unnamed', 'unnamedplus'}
+vim.opt.clipboard = {'unnamed', 'unnamedplus'}
 
 -- Whitespace
 
-vim.opt.wrap=true
+vim.opt.wrap = true
 
-vim.opt.textwidth=79
+vim.opt.textwidth = 79
 
-vim.opt.formatoptions='tcqrn1'
+vim.opt.formatoptions ='tcqrn1'
 
-vim.opt.tabstop=4
+vim.opt.tabstop = 4
 
-vim.opt.shiftwidth=4
+vim.opt.shiftwidth = 4
 
-vim.opt.softtabstop=4
+vim.opt.softtabstop = 4
 
-vim.opt.expandtab=true
+vim.opt.expandtab = true
 
 -- Cursor motion
 
-vim.opt.scrolloff=3
+vim.opt.scrolloff = 3
 
-vim.opt.backspace= {'indent', 'eol', 'start'}
+vim.opt.backspace = {'indent', 'eol', 'start'}
 
 -- Rendering
 
-vim.opt.ttyfast=true
+vim.opt.ttyfast = true
 
 -- Searching
 
-vim.opt.ignorecase=true
+vim.opt.ignorecase = true
 
-vim.opt.smartcase=true
+vim.opt.smartcase = true
 
-vim.opt.showmatch=true
+vim.opt.showmatch = true
 
