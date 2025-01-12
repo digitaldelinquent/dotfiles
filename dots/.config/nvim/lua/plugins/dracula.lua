635e47364e69ead74afbeb1d32d5a205aea8992e
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2025-01-12 11:04:40 -0800

    try not calling setup

--- dots/.config/nvim/lua/plugins/dracula.lua
+++ dots/.config/nvim/lua/plugins/dracula.lua
@@ -2,9 +2,6 @@ return {
     -- add dracula
     { "Mofiqul/dracula.nvim" },
     config = function()
-        local dracula = require("dracula")
-        dracula.setup()
-    
         -- Set theme
         vim.cmd.colorscheme('dracula')
     end
