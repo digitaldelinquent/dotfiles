6feee33b5959ff5616b2d3981578fecbea04f831
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2025-01-12 17:26:35 -0800

    set modifier to be space

--- dots/.config/nvim/lua/plugins/telescope.lua
+++ dots/.config/nvim/lua/plugins/telescope.lua
@@ -4,8 +4,8 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<space>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<space>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<space>fb', builtin.buffers, { desc = 'Telescope buffers' })
     end
 }
