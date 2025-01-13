291847685f297d1f72cc381913ace9a0fd384d70
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2025-01-12 17:11:02 -0800

    add missing return keyword

--- dots/.config/nvim/lua/plugins/blink-cmp.lua
+++ dots/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,4 +1,4 @@
-{
+return {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
     dependencies = 'rafamadriz/friendly-snippets',
@@ -7,10 +7,6 @@
     version = '*',
 
     opts = {
-        -- 'default' for mappings similar to built-in completion
-        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-        -- See the full "keymap" documentation for information on defining your own keymap.
         keymap = { preset = 'default' },
 
         appearance = {
