407185e53496675dd392c8dc824819f624092a23
digital_delinquent <6541669-digital_delinquent@users.noreply.gitlab.com>
2025-01-15 00:14:07 -0800

    remove rofi and dubst support

--- dots/.config/dunst/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q dunst
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x dunst >/dev/null; do sleep 1; done
-
-dunst &
