fab0ff62929571cccc280a5d23676cfff0dee9cc
Roel Mendoza <185047221+digitaldelinquent@users.noreply.github.com>
2025-09-28 11:00:56 -0700

    add compatibility for neovim 0.11

--- dots/.config/nvim/lua/plugins/lsp-config.lua
+++ dots/.config/nvim/lua/plugins/lsp-config.lua
@@ -12,16 +12,11 @@ return {
                 ensure_installed = {
                     "bashls",
                     "clangd",
-                    "gopls",
-                    "pyright",
+                    "ruff",
                     "sqlls",
                     "terraformls",
                     "elixirls",
-                    "helm_ls",
-                    "svelte",
-                    "eslint", 
                     "vimls", 
-                    "html"
                 }
             })
         end
@@ -29,7 +24,6 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            local lspconfig = require("lspconfig")
             local capabilities = require("blink.cmp").get_lsp_capabilities()
 
             local map = function(type, key, value)
@@ -43,25 +37,54 @@ return {
             end
 
             local lsp_servers = {
-                "bashls", "clangd", "gopls", "pyright", "sqlls", 
-                "terraformls", "elixirls", "helm_ls", "eslint", "vimls", "html"
+                "bashls", 
+                "clangd", 
+                "ruff", 
+                "sqlls", 
+                "terraformls", 
+                "elixirls",
+                "vimls",
             }
 
             -- Setup LSPs by iterating
             for _, lsp in ipairs(lsp_servers) do
+                vim.lsp.enable(lsp)
                 if lsp == "elixirls" then
-                    lspconfig[lsp].setup({
+                    vim.lsp.config(lsp, {
                         capabilities = capabilities,
                         cmd = { "language_server.sh" },
                         on_attach = custom_attach
                     })
                 else
-                    lspconfig[lsp].setup({
+                    vim.lsp.config(lsp, {
                         capabilities = capabilities,
                         on_attach = custom_attach
                     })
                 end
             end
+
+            vim.diagnostic.config({
+                virtual_text = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+                float = {
+                    border = "rounded",
+                    source = true,
+                },
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = "󰅚 ",
+                        [vim.diagnostic.severity.WARN] = "󰀪 ",
+                        [vim.diagnostic.severity.INFO] = "󰋽 ",
+                        [vim.diagnostic.severity.HINT] = "󰌶 ",
+                    },
+                    numhl = {
+                        [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+                        [vim.diagnostic.severity.WARN] = "WarningMsg",
+                    },
+                },
+            })
         end
     }
 }
