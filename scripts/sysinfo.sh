41fe432fa5883c43d8af4d28e1dd0087d78c00ed
digital_delinquent <185047221+digitaldelinquent@users.noreply.github.com>
2025-10-05 22:00:07 -0700

    remove unneeded scripts and consolidate into the niri config

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env zsh
 
-echo "$(date +"Date: %A %d %B %Y%nTime: %r")\nBattery: $(grep "POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT1/uevent | sed "s/POWER_SUPPLY_CAPACITY=//g")%" | xargs -d "\r" notify-send
+echo "$(date +"Date: %A %d %B %Y%nTime: %r")\nBattery: $(grep "POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT1/uevent \
+    | sed "s/POWER_SUPPLY_CAPACITY=//g")%" \
+    | xargs -d "\r" notify-send
