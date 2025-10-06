bdf6bac38267814c7404bed564bc807f34f09a95
Roel Mendoza <185047221+digitaldelinquent@users.noreply.github.com>
2025-10-05 19:16:11 -0700

    re-add dunst and rofi support

---
+++ dots/.config/dunst/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q dunst
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x dunst >/dev/null; do sleep 1; done
+
+dunst &
