34401d32ab270d760ae357777752f40154673054
digital_delinquent <185047221+digitaldelinquent@users.noreply.github.com>
2025-10-05 23:39:37 -0700

    change app name of sysinfo script

--- scripts/sysinfo.sh
+++ scripts/sysinfo.sh
@@ -2,4 +2,4 @@
 
 echo "$(date +"Date: %A %d %B %Y%nTime: %r")\nBattery: $(grep "POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT1/uevent \
     | sed "s/POWER_SUPPLY_CAPACITY=//g")%" \
-    | xargs -d "\r" notify-send
+    | xargs -d "\r" notify-send -a "Time and Battery"
